58bb91ac292686d7a9ba43aa32f6743f1c70bc30
CreatorLarry <lawrencengugi90@gmail>
2025-10-04 18:40:22 +0300

    trying clogs extention3

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@sanity/client";
 
 export const client = createClient({
-  projectId: "q0c3zitk",   // your project ID
+  projectId: "1v0dc0vw",   // your project ID
   dataset: "production",
   useCdn: true,            // `false` if you want fresh data
   apiVersion: "2023-10-01" // use a recent date
