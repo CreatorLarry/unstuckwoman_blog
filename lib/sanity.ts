31a43c66602710b05ab664d972bd8317daa6b2bb
CreatorLarry <lawrencengugi90@gmail>
2025-10-05 12:12:12 +0300

    finalizing extentions2

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,8 +1,8 @@
-import { createClient } from "@sanity/client";
+// import { createClient } from "@sanity/client";
 
-export const client = createClient({
-  projectId: "ct9pi7c932tek5do9pknsimw",   // your project ID
-  dataset: "production",
-  useCdn: true,            // `false` if you want fresh data
-  apiVersion: "2023-10-01" // use a recent date
-});
+// export const client = createClient({
+//   projectId: "ct9pi7c932tek5do9pknsimw",   // your project ID
+//   dataset: "production",
+//   useCdn: true,            // `false` if you want fresh data
+//   apiVersion: "2023-10-01" // use a recent date
+// });
