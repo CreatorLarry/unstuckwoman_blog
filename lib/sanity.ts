41fc4a3714919de1ced38bf211186198e16f3edd
CreatorLarry <lawrencengugi90@gmail>
2025-10-05 11:03:33 +0300

    finalizing extentions

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@sanity/client";
 
 export const client = createClient({
-  projectId: "1v0dc0vw",   // your project ID
+  projectId: "ct9pi7c932tek5do9pknsimw",   // your project ID
   dataset: "production",
   useCdn: true,            // `false` if you want fresh data
   apiVersion: "2023-10-01" // use a recent date
