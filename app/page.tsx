477d74754c50678bc5e85a03a5ad24d47ea78d05
CreatorLarry <lawrencengugi90@gmail>
2025-11-03 19:24:58 +0300

    testing4

--- app/page.tsx
+++ app/page.tsx
@@ -264,7 +264,7 @@ export default function Home() {
           href="https://whatsapp.com/channel/0029Vb6GO2q5PO0rVfgF4Q3g"
           target="_blank"
           rel="noopener noreferrer"
-          className="btn btn-success"
+          className="filter-btn active"
         >
           Join Our WhatsApp Channel
         </a>
